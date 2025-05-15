eb3d778cb3cfd78c6072da8b06b9d2c415b60be8
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-05-15 12:48:50 -1000

    configure devise-jwt

--- backend/config/initializers/devise.rb
+++ backend/config/initializers/devise.rb
@@ -310,4 +310,23 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+  # 
+  #
+
+  config.jwt do |jwt|
+    jwt.secret = Rails.application.credentials.fetch(:secret_key_base)
+
+    jwt.expiration_time = 1.day.to_i
+    
+    jwt.dispatch_requests = [
+      # devise/sessions#create
+      ['POST', %r{^/users/sign_in$}],
+      # devise/registrations#create
+      ['POST', %r{^/users$}]
+    ]
+    # jwt.revocation_requests = [
+    #   # devise/sessions#destroy
+    #   ['DELETE', %r{^/users?sign_out$}]
+    # ]
+  end
 end
