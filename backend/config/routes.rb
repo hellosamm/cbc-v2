d52b4b72d2312d2b811716443fdf46f07c50e457
hellosamm <142949327+hellosamm@users.noreply.github.com>
2025-05-19 14:32:50 -1000

    added routes to config/routes.rb

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,19 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
 }
 
+  namespace :api do
+    namespace :v1 do
+      resources :attendees, only: [:create, :destroy, :index]
+      get "user_events", to: "attendees#show"
+      get "/event/:event_id/attendee", to: "attendees#index", as: "event_attendees"
+      get "/event/:event_id/allAttendees", to: "attendees#list", as: "all_attendees"
+      resources :events do
+        get 'attendance_status', on: :member
+      end
+      resources :users, only: [:update]
+    end
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
